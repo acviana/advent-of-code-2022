fe7e7113765134a5838b378cc625cce01921ac4a
Alex C. Viana <alexcostaviana@gmail.com>
2022-12-31 05:51:56 -0600

    Updated template

--- advent_of_code_2022/template_module.py
+++ advent_of_code_2022/template_module.py
@@ -2,18 +2,25 @@ from typing import List
 
 
 def load_data() -> List[str]:
+    """Load data with minimal manipulation"""
     with open("inputs/day_X_input.txt") as f:
         return [item.strip() for item in f.readlines()]
 
 
 def parse_data(data: List[str]):
+    """Parse data upto the point where the final calculation can be
+    performed.
+    """
     pass
 
 
 def main() -> None:
+    """The final answer."""
     data = load_data()
     parse_data(data)
+    return None
 
 
 if __name__ == "__main__":
-    main()
+    answer = main()
+    print(f"The answer is {answer}")
